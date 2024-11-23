859bbdb25a4efa6abeb005468cb4be00b72caf8b
eng-Anas-Osama <enganasosamaahmed@gmail.com>
2024-11-23 22:35:47 +0200

    Making the code cleaner and more logical, 50sections finished

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<?php get_header(); ?> 
+<?php 
+  get_header(); 
+  pageBanner(array(
+    'title' => 'Welcome To our Blog',
+    'subtitle' => 'Keep up for our latest news.'
+  ));
+?> 
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)">
-      </div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcom to our blog!</h1>
-        <div class="page-banner__intro">
-          <p>Keep up for our latest news.</p>
-        </div>
-      </div>
-</div>
     <div class="container container--narrow page-section">
       <?php
         while (have_posts()){
